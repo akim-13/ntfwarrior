75f7b7d03a64e1bbbbb0e4db6838b9132a73d76f
Akim <akim.k@tutamail.com>
2023-09-17 16:04:34 +0100

    Added notification buttons, fixed listing, minor changes

--- cmd
+++ cmd
@@ -1,7 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 TASKRC="/data/data/com.termux/files/home/.config/taskwarrior/taskrc"
+# Cool bash 4.0+ feature to make $1 lowercase
+input="${1,,}"
+output=$(eval "task rc:$TASKRC $input" 2>&1)
 
-input=$1
+/data/data/com.termux/files/home/.config/taskwarrior/start_daemon "$output"
 
-eval "task rc:$TASKRC $input"
+#termux-notification --title "Daemon" --action "bash -lc 'am start --user 0 -n com.termux/com.termux.app.TermuxActivity && $XDG_CONFIG_HOME/taskwarrior/start_daemon'" --button1 "Add" --button1-action "bash -lc '$XDG_CONFIG_HOME/taskwarrior/new_task \$REPLY'" --button2 "List" --button2-action "bash -lc '$XDG_CONFIG_HOME/taskwarrior/list_tasks'" --button3 "CMD" --button3-action "bash -lc '$XDG_CONFIG_HOME/taskwarrior/cmd \$REPLY'" --content "$output" --id 1 --ongoing --alert-once --group "daemon"
--- hooks/on-add.add-ntf.sh
+++ hooks/on-add.add-ntf.sh
@@ -1,17 +1,21 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 XDG_DATA_HOME="/data/data/com.termux/files/home/.local/share"
+rc="/data/data/com.termux/files/home/.config/taskwarrior/taskrc"
 
 read new_task
 
 # Check if the required fields are present in the JSON data
-if ! echo $new_task | jq 'has("uuid") and has("description") and has("ntf1")' | grep -q true; then
-    termux-toast "ERROR: one or more fields are missing."
+if ! echo $new_task | jq 'has("uuid") and has("description") and has("ntf1") and has("due")' | grep -q true; then
+    echo
+    echo "ERROR: can't process hook, missing fields."
     exit 1
 fi
 
 uuid=$(echo $new_task | jq -r .uuid )
 desc=$(echo $new_task | jq -r .description )
+# E.g. 23:00 on 17/09
+due=$(echo $new_task | jq -r .due | awk '{printf "%s:%s on %s/%s\n", substr($0, 10,2), substr($0, 13,2), substr($0,5,2), substr($0,7,2)}')
 
 
 # E.g. 20230709T151010Z
@@ -25,18 +29,22 @@ period_ms=$(((ntf1_epoch - cur_epoch)*1000))
 ntf_file="$XDG_DATA_HOME/taskwarrior/ntfs/$uuid"
 echo "if [ \"\$(date -u +%s)\" -lt \"$ntf1_epoch\" ]; then exit 0; fi" > $ntf_file
 
-echo "termux-notification --content \"$desc\"" >> $ntf_file
 # int from 1 to 32767
-job_id=$RANDOM
-echo "termux-job-scheduler --cancel --job-id $job_id" >> $ntf_file
+rand_id=$RANDOM
+
+# For testing
+#termux-notification --title "$desc" --content "Due at $due" --button1 "Start/Stop" --button1-action "task rc:$rc +ACTIVE _uuids | grep -q \"^$uuid$\" && (task rc:$rc stop $uuid && termux-toast Task stopped) || (task rc:$rc start $uuid && termux-toast Task started)" --button2 "Done" --button2-action "task rc:$rc done $uuid && termux-notification-remove $rand_id" --id $rand_id --ongoing
+
+echo "termux-notification --title \"$desc\" --content \"Due at $due\" --button1 \"Start/Stop\" --button1-action \"task rc:$rc +ACTIVE _uuids | grep -q '^'\"$uuid\"'$' && (task rc:$rc stop $uuid && termux-toast Task stopped) || (task rc:$rc start $uuid && termux-toast Task started)\" --button2 \"Done\" --button2-action \"task rc:$rc done $uuid && termux-notification-remove $rand_id\" --id $rand_id --ongoing" >> $ntf_file
+#echo "termux-notification --title \"$desc\" --content \"Due $due\" --button1 \"Start/Stop\" --button1-action \"echo\" --button2 \"Done\" --button2-action \"task rc:$rc done $uuid && termux-notification-remove $rand_id\" --id $rand_id --ongoing" >> $ntf_file
+
+echo "termux-job-scheduler --cancel --job-id $rand_id" >> $ntf_file
 chmod +x $ntf_file
 
-termux-job-scheduler --script $ntf_file --job-id $job_id --period-ms $period_ms --persisted true --battery-not-low false > /dev/null
+termux-job-scheduler --script $ntf_file --job-id $rand_id --period-ms $period_ms --persisted true --battery-not-low false > /dev/null
 
 # Task json
 echo $new_task
 # Feedback (terminal output)
-echo Task with notification created
-#termux-toast "Task created"
+echo
 
-# termux-notification --button1 "New task" --button1-action "termux-toast \$REPLY" --id 1 --ongoing --title Daemon --alert-once
